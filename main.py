2ae31640f5eed30d077bd434efad261740b46d5e
brnpal <brendan.allaway@gmail.com>
2024-10-02 12:14:42 -0700

    black screen

--- main.py
+++ main.py
@@ -4,10 +4,25 @@
 import pygame
 from constants import *
 
+
+
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")      
+
+    #draw game to screen
+    while True:
+        # make window close button work
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        # make black screen and refresh
+        screen.fill((0,0,0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()

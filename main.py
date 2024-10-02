74050bf2f51b3eed24d80ec3d35b5a5bdb1cf2dd
brnpal <brendan.allaway@gmail.com>
2024-10-02 14:03:03 -0700

    triangle on screen

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")      
+    clock = pygame.time.Clock()
+    dt = 0 
 
-    #draw game to screen
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    ## draw game to screen ##
     while True:
+
         # make window close button work
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
-            
-        # make black screen and refresh
-        screen.fill((0,0,0))
+                return 
+    
+        # draw and refresh    
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
-
-#
